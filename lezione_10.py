35f65c7174d55b014ffcd99d6cfe98a8252f9c95
Umberto Emanuele <info@emanueleumberto.it>
2026-04-30 11:23:45 +0200

    OOP Incapsulamento e traccia esercizio

--- lezione_10.py
+++ lezione_10.py
@@ -51,6 +51,17 @@
 # self._prop    -> protected
 # self.__prop   -> private
 
+# Vantaggi dell’incapsulamento
+# Protegge i dati
+# Riduce bug
+# Migliora la manutenzione
+# Permette validazione automatica
+# Facilita il refactoring
+
+# Errori comuni
+# Usare variabili pubbliche sempre
+# Usare get_ e set_ come in Java -> In Python è meglio: @property
+
 class Automobile:
     
     totale = 0
@@ -114,8 +125,10 @@ class ContoCorrente:
     # Metodo di istanza
     def info(self):
         print(f"Conto corrente di {self.__nome} {self.__cognome} Saldo: {self.__saldo}")
-    
-    # Property
+        
+    # Metodo migliore: usare le 'property'
+    # Le 'property' sono il modo più Pythonico per fare incapsulamento.
+    #Esempio con @property
     @property
     def saldo(self):
         return self.__saldo
@@ -123,10 +136,71 @@ class ContoCorrente:
     @saldo.setter
     def saldo(self, importo):
         if importo <= 0:
-            raise ValueError("Importo non valido!!!")
+            print("Non puoi aggiungere importi negativi")
+            return
         self.__saldo = importo        
     
 c = ContoCorrente("Mario", "Rossi")
-c.saldo = 200 # Sto utilizzando il metodo Setter
+c.saldo = -25 # Sto utilizzando il metodo Setter
 print(c.saldo) # Sto utilizzando il metodo Getter
 c.info()
+
+print('##############################################################')
+
+# Esempio pratico
+class Studente:
+    
+    # Attributo di classe
+    __contatore_matricola = 0
+    
+    def __init__(self, nome):
+        self.__matricola = self.__genera_matricola()
+        self.__nome = nome
+        self.__voti = []
+        
+    @property
+    def nome(self):
+        return self.__nome
+    
+    @property
+    def voti(self):
+        return self.__voti
+        
+    # Metodi di istanza
+    def info(self):
+        return f'Studente {self.__nome} matricola: {self.__matricola} voti: {self.__voti}'
+
+    # Metodi di istanza
+    def aggiungi_voto(self, voto):
+        if voto > 0 and voto <= 30:
+            self.__voti.append(voto)
+        else:
+            print('Hai aggiunto un valore errato')
+    
+    # Metodo di classe
+    @classmethod
+    def __genera_matricola(cls):
+        cls.__contatore_matricola += 1
+        matricola = cls.__contatore_matricola 
+        if matricola < 10:
+            matricola = f'US000{matricola}'
+        elif matricola < 100:
+            matricola = f'US00{matricola}'
+        elif matricola < 1000:
+            matricola = f'US0{matricola}'
+        else:
+            matricola = f'US{matricola}'
+        return matricola
+    
+    
+s1 = Studente('Mario Rossi')
+s2 = Studente('Giuseppe Verdi')
+s3 = Studente('Francesca Neri')
+# Errore logico richiamare il metodo genera_matricola() al di fuori della classe studente
+# s.genera_matricola() 
+
+s1.aggiungi_voto(30)
+s1.aggiungi_voto(25)
+s1.aggiungi_voto(18)
+
+print(s1.info())
